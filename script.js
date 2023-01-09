b9148361c0b5a02ad76215434fd1c4f9dbb1ac53
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-01-08 21:34:10 -0800

    Add win condition checking

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const p2 = Player('Kevin', 'O');
 
 const board = (() => {
   const boardArr = [];
+  let lastEdit = { row: 0, col: 0, marker: '' };
 
   for (let row = 0; row < 3; row++) {
     boardArr.push([]);
@@ -21,9 +22,39 @@ const board = (() => {
 
   const setCell = (row, col, marker) => {
     boardArr[row][col] = marker;
+    lastEdit = Object.assign(lastEdit, { row, col, marker });
   };
 
-  return { getBoard, setCell, getCell };
+  const checkWin = () => {
+    for (let row = 0; row < boardArr.length; row++) {
+      for (let col = 0; col < boardArr[row].length; col++) {
+        if (lastEdit.marker !== boardArr[row][col]) break;
+        if (col + 1 === boardArr.length) return true;
+      }
+    }
+
+    for (let col = 0; col < boardArr.length; col++) {
+      for (let row = 0; row < boardArr.length; row++) {
+        if (lastEdit.marker !== boardArr[row][col]) break;
+        if (row + 1 === boardArr.length) return true;
+      }
+    }
+
+    if (
+      (boardArr[0][0] === lastEdit.marker &&
+        boardArr[1][1] === lastEdit.marker &&
+        boardArr[2][2] === lastEdit.marker) ||
+      (boardArr[0][2] === lastEdit.marker &&
+        boardArr[1][1] === lastEdit.marker &&
+        boardArr[2][0])
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  return { checkWin, getBoard, setCell, getCell };
 })();
 
 const game = (() => {
@@ -55,6 +86,10 @@ const game = (() => {
   );
 
   const endTurn = () => {
+    if (board.checkWin()) {
+      console.log('You Win!');
+    }
+
     if (currPlayer === p1) {
       currPlayer = p2;
     } else {
